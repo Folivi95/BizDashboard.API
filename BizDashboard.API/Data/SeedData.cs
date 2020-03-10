d7575e0f9261c6a122da940a543f1bd04aeca048
joshuafolivi <joshua.folivi@gmail.com>
2020-03-10 19:20:11 +0100

    Seeding Database

--- BizDashboard.API/Data/SeedData.cs
+++ BizDashboard.API/Data/SeedData.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace BizDashboard.API.Data
 {
@@ -17,22 +16,160 @@ namespace BizDashboard.API.Data
 
         public void DataSeed(int nCustomers, int nOrders)
         {
-            if (this.ctx.Customers.Any())
+            if (!this.ctx.Customers.Any())
             {
                 SeedCustomers(nCustomers);
             }
 
-            if (this.ctx.Orders.Any())
+            if (!this.ctx.Orders.Any())
             {
                 SeedOrders(nCustomers);
             }
 
-            if (this.ctx.Servers.Any())
+            if (!this.ctx.Servers.Any())
             {
-                SeedServers(nCustomers);
+                SeedServers();
             }
 
             this.ctx.SaveChanges();
+        }
+
+        private void SeedCustomers(int n)
+        {
+            List<Customer> customers = BuildCustomerList(n);
+
+            foreach (var customer in customers)
+            {
+                this.ctx.Customers.Add(customer);
+            }
+        }
+
+        public void SeedOrders(int n)
+        {
+            List<Order> orders = BuildOrderList(n);
+
+            foreach (var order in orders)
+            {
+                this.ctx.Orders.Add(order);
+            }
+        }
+
+        private List<Order> BuildOrderList(int n)
+        {
+            var orders = new List<Order>();
+            var rand = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                var placed = Helpers.GetRandomOrderPlaced();
+                var completed = Helpers.GetRandomOrderCompleted(placed);
+
+                var randCustomerId = rand.Next(this.ctx.Customers.Count());
+
+                orders.Add(new Order
+                {
+                    Id = i,
+                    Customer = this.ctx.Customers.First(c => c.Id == randCustomerId),
+                    Total = Helpers.GetRandomOrderTotal(),
+                    Placed = placed,
+                    Completed = completed
+                });
+            }
+
+            return orders;
+        }
+
+        public void SeedServers()
+        {
+            List<Server> servers = BuildServerList();
+
+            foreach (var server in servers)
+            {
+                this.ctx.Servers.Add(server);
+            }
+        }
+
+        private List<Server> BuildServerList()
+        {
+            return new List<Server>()
+            {
+                new Server
+                {
+                    Id = 1,
+                    Name = "Dev-Web",
+                    IsOnline = true
+                },
+                new Server
+                {
+                    Id = 2,
+                    Name = "Dev-Mail",
+                    IsOnline = true
+                },
+                new Server
+                {
+                    Id = 3,
+                    Name = "Dev-Services",
+                    IsOnline = false
+                },
+                new Server
+                {
+                    Id = 4,
+                    Name = "QA-Web",
+                    IsOnline = true
+                },
+                new Server
+                {
+                    Id = 5,
+                    Name = "QA-Mail",
+                    IsOnline = true
+                },
+                new Server
+                {
+                    Id = 6,
+                    Name = "Qa-Services",
+                    IsOnline = false
+                },
+                new Server
+                {
+                    Id = 7,
+                    Name = "Prod-Web",
+                    IsOnline = true
+                },
+                new Server
+                {
+                    Id = 8,
+                    Name = "Prod-Mail",
+                    IsOnline = false,
+                },
+                new Server
+                {
+                    Id = 9,
+                    Name = "Prod-Services",
+                    IsOnline = true
+                },
+            };
+        }
+
+        private List<Customer> BuildCustomerList(int nCustomers)
+        {
+            var customers = new List<Customer>();
+            var names = new List<string>();
+
+            for (int i = 0; i < nCustomers; i++)
+            {
+                var name = Helpers.MakeUniqueCustomerName(names);
+                names.Add(name);
+
+                customers.Add(new Customer
+                {
+                    Id = i,
+                    Name = name,
+                    Email = Helpers.MakeCustomerEmail(name),
+                    State = Helpers.GetRandomState()
+                });
+            }
+
+            return customers;
         }
     }
 }
--- BizDashboard.API/Startup.cs
+++ BizDashboard.API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BizDashboard.API.Data;
 using BizDashboard.API.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -27,16 +28,18 @@ namespace BizDashboard.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
             services.AddControllers();
+            services.AddMvc();
             services.AddEntityFrameworkNpgsql().AddDbContext<ApiContext>(opt =>
             {
                 opt.UseNpgsql(Configuration.GetConnectionString("secretConnectionString"));
             });
+
+            services.AddTransient<SeedData>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, SeedData data)
         {
             if (env.IsDevelopment())
             {
@@ -48,6 +51,8 @@ namespace BizDashboard.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            data.DataSeed(20, 1000);
 
             app.UseEndpoints(endpoints =>
             {
