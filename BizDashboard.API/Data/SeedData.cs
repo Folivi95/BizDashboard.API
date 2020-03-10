28daf43d91dfc3970238418a018312c9d79793a6
joshuafolivi <joshua.folivi@gmail.com>
2020-03-10 12:45:03 +0100

    Add project files.

---
+++ BizDashboard.API/Data/SeedData.cs
@@ -0,0 +1,38 @@
+﻿using BizDashboard.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BizDashboard.API.Data
+{
+    public class SeedData
+    {
+        private readonly ApiContext ctx;
+
+        public SeedData(ApiContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        public void DataSeed(int nCustomers, int nOrders)
+        {
+            if (this.ctx.Customers.Any())
+            {
+                SeedCustomers(nCustomers);
+            }
+
+            if (this.ctx.Orders.Any())
+            {
+                SeedOrders(nCustomers);
+            }
+
+            if (this.ctx.Servers.Any())
+            {
+                SeedServers(nCustomers);
+            }
+
+            this.ctx.SaveChanges();
+        }
+    }
+}
